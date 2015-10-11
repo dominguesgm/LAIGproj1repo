a845e99ae698df078f3d45927729732eeea225b5
Gil Domingues <gmdz95@gmail.com>
2015-10-11 23:31:40 +0100

    TODO: amplification factors

--- reader/MyTriangle.js
+++ reader/MyTriangle.js
@@ -27,6 +27,27 @@ function MyTriangle(scene, coordinates) {
 	this.y3 = coordinates[7];
 	this.z3 = coordinates[8];
 
+	this.a = Math.sqrt((this.x1 - this.x3) * (this.x1 - this.x3) + 
+			 		   (this.y1 - this.y3) * (this.y1 - this.y3) +
+			 		   (this.z1 - this.z3) * (this.z1 - this.z3));
+
+	this.b = Math.sqrt((this.x2 - this.x1) * (this.x2 - this.x1) + 
+			 		   (this.y2 - this.y1) * (this.y2 - this.y1) +
+			 		   (this.z2 - this.z1) * (this.z2 - this.z1));
+
+	this.c = Math.sqrt((this.x3 - this.x2) * (this.x3 - this.x2) + 
+			 		   (this.y3 - this.y2) * (this.y3 - this.y2) +
+			 		   (this.z3 - this.z2) * (this.z3 - this.z2));
+
+	this.cosAlpha = (-this.a*this.a + this.b*this.b + this.c * this.c) / (2 * this.b * this.c);
+	this.cosBeta =  ( this.a*this.a - this.b*this.b + this.c * this.c) / (2 * this.a * this.c);
+	this.cosGamma = ( this.a*this.a + this.b*this.b - this.c * this.c) / (2 * this.a * this.b);
+				
+	this.beta = Math.acos(this.cosBeta);
+	this.alpha = Math.acos(this.cosAlpha);
+	this.gamma = Math.acos(this.cosGamma);
+	this.sum = this.beta + this.alpha + this.gamma;
+
 
 	this.initBuffers();
 };
@@ -53,7 +74,10 @@ MyTriangle.prototype.initBuffers = function () {
 	
 	
 	this.texCoords = [
-	];
+		0.0, 1,
+		this.c, 1.0,
+	  (this.c - this.a * Math.cos(this.beta)), 0.0
+    ];
 
 	this.primitiveType=this.scene.gl.TRIANGLES;	
 	this.initGLBuffers();
